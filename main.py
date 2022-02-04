54d5a120904dcc3f3c84ee686a279680ec062f00
Will Noland <therewillbnoland@gmail.com>
2022-02-03 18:10:48 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        para_dict = {'(': ')', '{': '}', '[': ']'}
+        par_dict = {'(': ')', '{': '}', '[': ']'}
         opcl = []
         for i in range(len(s)):
             if len(opcl) > 0:
-                if para_dict[opcl[-1]] == s[i]:
+                if par_dict[opcl[-1]] == s[i]:
                     del opcl[-1]
-                elif s[i] in para_dict:
+                elif s[i] in par_dict:
                     opcl.append(str(s[i]))
-                elif para_dict[opcl[-1]] != s[i]:
+                elif par_dict[opcl[-1]] != s[i]:
                     return False
-            elif len(opcl) == 0 and s[i] not in para_dict:
+            elif len(opcl) == 0 and s[i] not in par_dict:
                 return False
             else:
                 opcl.append(str(s[i]))
